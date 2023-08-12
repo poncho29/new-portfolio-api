ae07fa9d1555fbeb6f7c5fcce9a5dddfae253d24
Sebastian Meneses <meneses0828@gmail.com>
2023-08-12 14:49:23 -0500

    [add] - create project service

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,18 @@
-export class CreateProjectDto {}
+import { IsString, MinLength, IsOptional, IsArray } from 'class-validator';
+
+export class CreateProjectDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsString()
+  description: string;
+
+  @IsString({ each: true }) // each en true obliga a que sean strings
+  @IsArray()
+  tags: string[];
+}
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Project } from './entities/project.entity';
+
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 
 @Injectable()
 export class ProjectsService {
-  create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectRepository: Repository<Project>,
+  ) {}
+
+  async create(createProjectDto: CreateProjectDto) {
+    try {
+      const project = this.projectRepository.create(createProjectDto);
+
+      await this.projectRepository.save(project);
+      return project;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Ayuda!');
+    }
   }
 
   findAll() {
